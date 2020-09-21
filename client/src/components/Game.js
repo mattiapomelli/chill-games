f70551af03bd6c840fce4a75167298dd4867e57b
mattiapomelli <mattiapomelli@gmail.com>
2020-09-21 19:49:43 +0200

    quit game button

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -2,34 +2,12 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 const Game = () => {
-    //const {gameOver, setGameOver, setFinalScore, setStats} = useContext(GameContext)
-    //const {isAuthenticated, loggedUser, setLoggedUser} = useContext(AuthContext)
-    //const [gameLaunched, setGameLaunched] = useState(false)
-
-
-    // const endGame = (score, gameStats) => {
-    //   setFinalScore(score)
-    //   setStats(gameStats)
-    //   setGameOver(true)
-    //   if(isAuthenticated){
-    //     updateUserScore(score, gameStats)
-    //   }
-    // }
-
-    // const updateUserScore = (score, stats) => {
-
-    //   //if(score > loggedUser.bestScore) {
-    //   axios.put(`/user/${loggedUser._id}`, {
-    //     score: score,
-    //     stats: stats
-    //   }).then(res => {
-    //     if(score > loggedUser.bestScore)
-    //       setLoggedUser({...loggedUser, bestScore: score})
-    //   })
-
 
     return (
-          <Link to="/zombiegame">Play</Link>
+      <div>
+        <h1>Zombie Game</h1>
+        <Link to="/zombiegame">Play</Link>
+      </div>
     )
 }
 
