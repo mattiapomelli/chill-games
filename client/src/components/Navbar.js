c7cab48daa77062a350c3608021a0488f4a1047a
mattiapomelli <mattiapomelli@gmail.com>
2020-09-22 14:37:43 +0200

    navbar css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,38 +1,40 @@
 import React, {useContext} from "react"
 import { AuthContext } from "../context/AuthContext"
-import { GameContext } from "../context/GameContext"
-import AuthService from "../services/AuthService"
 import { Link } from "react-router-dom"
+import "../css/navbar.css"
 
 
 const Navbar = () => {
-    const {isAuthenticated, loggedUser, setIsAuthenticated, setLoggedUser } = useContext(AuthContext)
-    const {setFinalScore} = useContext(GameContext)
-
-    const logOut = () => {
-		AuthService.logout().then(data => {
-			if(data.success){
-				setFinalScore(0)
-				setLoggedUser(data.user)        
-				setIsAuthenticated(false)
-			}
-		})
-	}
+    const {isAuthenticated, loggedUser} = useContext(AuthContext)
 
     return (
         <div>
-            {isAuthenticated ?
-				<div>
-					<span><Link to={`/user/${loggedUser._id}`}>{loggedUser.username}</Link></span>
-					<span>Your record is: {loggedUser.bestScore}</span>
-					<button onClick={logOut}>Logout</button>
-				</div>
-			: <div>
-				<span>Guest</span>
-				<Link to="/login">Login</Link>
-				<Link to="/ranking">Ranking</Link>
-				<Link to="/">Play</Link>
-			</div>}
+			<div className="header-container">
+				<header>
+					{/* <span>Your record is: {loggedUser.bestScore}</span> */}
+
+					<Link className="menu-item" to="/ranking">Ranking</Link>
+
+					<Link className="menu-item" to="/">Play</Link>
+
+					{
+						isAuthenticated?
+
+					<div className="user-info">
+						<img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="profile"/>
+						<Link className="user-name" to={`/user/${loggedUser._id}`}>{loggedUser.username}</Link>
+					</div>
+					:
+					<div className="navbar-right">
+						<div className="user-info">
+							<img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="profile"/>
+							<span className="user-name">Guest</span>
+						</div>
+						<Link className="navbar-login-btn" to="/login">Login</Link>
+					</div>
+					}
+				</header>			
+			</div>
         </div>
     )
 }
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,12 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useContext} from "react"
+import { GameContext } from "../context/GameContext"
+import { AuthContext } from "../context/AuthContext"
+import AuthService from "../services/AuthService"
 import axios from "axios"
 
 const Profile = (props) => {
+    const {setIsAuthenticated, setLoggedUser } = useContext(AuthContext)
+    const {setFinalScore} = useContext(GameContext)
     const [user, setUser] = useState({})
 
     useEffect(() => {
@@ -11,9 +16,20 @@ const Profile = (props) => {
         }).catch(err => console.log(err))
     }, [props.match.params.id]) //so when we go from an user profile to another user's profile since props change the component re-fetches data and re-mounts
 
+    const logOut = () => {
+		AuthService.logout().then(data => {
+			if(data.success){
+				setFinalScore(0)
+				setLoggedUser(data.user)        
+				setIsAuthenticated(false)
+			}
+		})
+	}
+
     return (
         <div>
             <h1>{user.username}</h1>
+            <button onClick={logOut}>Logout</button>
         </div>
     )
 }
