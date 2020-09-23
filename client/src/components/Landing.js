2ba5d6df89e43c11347ca9553a06f6bd9e249b1b
mattiapomelli <mattiapomelli@gmail.com>
2020-09-23 21:34:05 +0200

    landing page css

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,15 +1,25 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "../css/landing.css"
 
 const Landing = () => {
 
     return (
-      <div>
-        <h1>Zombie Game</h1>
-        <Link to="/zombiegame">Play</Link>
-        <h1>Car Game</h1>
-        <Link to="/cargame">Play</Link>
-      </div>
+		<div className="landing-container">
+			<div className="games-container">
+				<div className="poster-container">
+					<img src="/images/covers/zombiegame.jpg" alt="game poster"></img>
+					<h1>Zombie Game</h1>
+					<Link to="/zombiegame">Play</Link>
+				</div>
+				
+				<div className="poster-container">
+					<img src="/images/covers/cargame.jpg" alt="game poster"></img>
+					<h1>Car Game</h1>
+					<Link to="/cargame">Play</Link>
+				</div>
+			</div>
+		</div>
     )
 }
 
